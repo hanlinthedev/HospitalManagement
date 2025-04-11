b58582fba9c831380e9ad3642d1f3c1adbf262fc
HanlinTheDEVELOPER <hanlinthedeveloper@gmail.com>
2025-04-11 22:37:38 +0630

    add api routes docs

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -57,4 +57,9 @@ class User extends Authenticatable implements JWTSubject
             'password' => 'hashed',
         ];
     }
+
+    public function profile()
+    {
+        return $this->hasOne(UserProfile::class);
+    }
 }
