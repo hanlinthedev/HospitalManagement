6e98b285563624290f08e03961f05a91480e838f
HanlinTheDEVELOPER <hanlinthedeveloper@gmail.com>
2025-04-12 21:04:26 +0630

    how it work section in front end

--- user/src/components/home/howItWork.tsx
+++ user/src/components/home/howItWork.tsx
@@ -1,20 +1,26 @@
-import chooseADoctor from "@/assets/chooseDoctor.png";
-import confirmBooking from "@/assets/confirmBooking.png";
-import pickADate from "@/assets/pickADate.png";
+import confirm from "@/assets/confirm.svg";
+import date from "@/assets/date.svg";
+import doctor from "@/assets/doctor.svg";
 
 const HowItWork = () => {
 	return (
 		<section className="p-4 sm:px-12">
 			<h1 className=" font-semibold text-2xl text-primary">How it works</h1>
-			<div className="flex flex-col md:flex-row">
+			<div className="flex flex-col md:flex-row justify-between items-center gap-4 mt-12 px-12">
 				<div>
-					<img src={chooseADoctor} alt="" />
+					<div className="bg-primary w-52 h-52 rounded-full flex justify-center items-center">
+						<img src={doctor} alt="" />
+					</div>
 				</div>
 				<div>
-					<img src={pickADate} alt="" />
+					<div className="bg-primary w-52 h-52 rounded-full flex justify-center items-center">
+						<img src={date} alt="" />
+					</div>
 				</div>
 				<div>
-					<img src={confirmBooking} alt="" />
+					<div className="bg-primary w-52 h-52 rounded-full flex justify-center items-center">
+						<img src={confirm} alt="" />
+					</div>
 				</div>
 			</div>
 		</section>
